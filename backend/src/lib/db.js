8cead7786db88a9ae5d7d137b60ad1afd922c96d
Siddhant Patel <spatel44427777@gmail.com>
2025-10-15 23:19:28 +0530

    env vars correction

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -5,7 +5,7 @@ export const connectDB = async () => {
     const { MONGO_URI } = ENV;
     if (!MONGO_URI) throw new Error("MONGO_URI is not set");
 
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(ENV.MONGO_URI);
     console.log("MONGODB CONNECTED:", conn.connection.host);
   } catch (error) {
     console.error("Erronr to connect mongoDB: ", error);
